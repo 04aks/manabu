3f56c9ead0108a3fcafa20d0e8582079de170f26
04aks <abdellahssehib@gmail.com>
2024-12-01 17:34:07 +0100

    minor changes

--- src/main/java/aks/manabu/service/ManaService.java
+++ src/main/java/aks/manabu/service/ManaService.java
@@ -22,7 +22,7 @@ public class ManaService {
         if(getMangaPanel() != null){
             GeminiClient client = new GeminiClient();
             client.authenticate(System.getenv("GEMINI-KEY"));
-            client.requestGemini("What do you see in the image? ", getMangaPanel().getEncodedImage());
+            client.requestGemini("create a json string that has a sentence array key that contains dialogues from the manga panel in the image, a translated_sentences key where you translate the sentences from japanese to english, do NOT flag the response with '```json ```' it must be a straight line string, as for the order of the sentences; it should be in japanese manga order", getMangaPanel().getEncodedImage());
             return client.responseGemini();
         }
         return null;
